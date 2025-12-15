bfcb301b961120e9f4b347c811a45ebbbb01d603
CChangala <cchangala@umass.edu>
2025-12-14 20:14:32 -0600

    extract- load

--- scripts/extract-load-raw.py
+++ scripts/extract-load-raw.py
@@ -4,12 +4,12 @@ import requests
 from google.cloud import bigquery
 from io import BytesIO
 
-DATA_URL = ""
-RAW_FILE_PATH =""
-TABLE_ID = ""
-DATASET_ID = ""
-PROJECT_ID = ""
-CREDENTDIALS_PATH = ""
+
+RAW_FILE_PATH = "data/sdud-2025-updated-dec2025.csv" # <-- NEW FILE NAME
+TABLE_ID = "cms_utilization_raw"
+DATASET_ID = "raw_data"
+PROJECT_ID = "pharmacy-pipeline-2025"
+CREDENTDIALS_PATH = "secrets/bigquery-editor-key.json"
 
 """MAP OF CONTENTS: 
 1. Extract data from a URL
@@ -21,9 +21,9 @@ def load_to_bigquery(df):
     client = bigquery.Client(project = PROJECT_ID)
     table_ref = client.dataset(DATASET_ID).table(TABLE_ID)
 
-    job_config = bigquery.LoadJobConfig(auto_detect =True,
-                                        write_disposition="WRITE_TRUNCATE",
-                                        null_marker = "")
+    job_config = bigquery.LoadJobConfig(autodetect=True,
+                                    write_disposition="WRITE_TRUNCATE")
+    
     print(f"Loading data into BigQuery table {TABLE_ID}...")
     job = client.load_table_from_dataframe(df, table_ref, job_config=job_config)
     job.result()  # Wait for the job to complete.
@@ -32,18 +32,14 @@ def load_to_bigquery(df):
 
 
 def extract_and_load_data():
-    print("Starting data extraction from {DATA_URL}...")
-    os.makedirs("data", exist_ok=True)
+    if not os.path.exists(RAW_FILE_PATH):
+        print(f"!!! ERROR: Data file not found at {RAW_FILE_PATH}")
+        print("Please ensure the downloaded CSV is named 'raw_sdud_data.csv' and is in the 'data/' folder.")
+        return
 
-    response = requests.get(DATA_URL,stream=True, timeout=300)
-    response.raise_for_status()
+    print(f"Starting data load from local file: {RAW_FILE_PATH}")
 
-    with open(RAW_FILE_PATH, 'wb') as f:
-        for chunk in response.iter_content(chunk_size=8192):
-            f.write(chunk)
-
-    print(f"Successfully downloaded file to {RAW_FILE_PATH}")
-
+    # Read the CSV file into a DataFrame
     df = pd.read_csv(RAW_FILE_PATH, low_memory=False)
     
     # Load into BigQuery
